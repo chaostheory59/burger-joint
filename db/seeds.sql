4e65e298b840b65aefd3c359f98340554b7d6564
Andrew Vann <51332669+chaostheory59@users.noreply.github.com>
2019-08-25 11:16:07 -0400

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Fried Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon and Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Steak Burger');
+
